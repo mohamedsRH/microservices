ed77fe64b56683b566fc6991230f466f12a2fa3f
Mohamed SALLAMI RHOUMA <msallamirhouma@sofrecom.fr>
2024-07-17 10:01:37 +0100

    init microservices book and library

---
+++ LibrarayMicroservice/src/main/java/com/microservices/libraraymicroservice/LibrarayMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.libraraymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarayMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( LibrarayMicroserviceApplication.class, args );
+    }
+
+}
---
+++ LibrarayMicroservice/src/test/java/com/microservices/libraraymicroservice/LibrarayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.libraraymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarayMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
